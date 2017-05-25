5299f034225279e6464528b6ca2866eae9e8649e
Adriana <aao317@hotmail.com>
2017-05-25 07:51:13 -0500

    Diseño pantallas de consejos

--- app/src/main/java/com/example/eduardopalacios/myapp/Consejo_Compras.java
+++ app/src/main/java/com/example/eduardopalacios/myapp/Consejo_Compras.java
@@ -1,13 +1,25 @@
 package com.example.eduardopalacios.myapp;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Consejo_Compras extends AppCompatActivity {
-
+TextView title_consejo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consejo__compras);
+        title_consejo= (TextView) findViewById(R.id.title_consejo2);
+        cambiar_letra();
     }
+
+
+    public void cambiar_letra(){
+        Typeface face= Typeface.createFromAsset(getAssets(),"fonts/Langdon.otf");
+        title_consejo.setTypeface(face);
+
+    }
+
 }
--- app/src/main/java/com/example/eduardopalacios/myapp/Consejo_TiempoAhorrar.java
+++ app/src/main/java/com/example/eduardopalacios/myapp/Consejo_TiempoAhorrar.java
@@ -1,13 +1,24 @@
 package com.example.eduardopalacios.myapp;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Consejo_TiempoAhorrar extends AppCompatActivity {
-
+TextView title_consejo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consejo__tiempo_ahorrar);
+        title_consejo= (TextView) findViewById(R.id.title_consejo4);
+        cambiar_letra();
+    }
+
+
+    public void cambiar_letra(){
+        Typeface face= Typeface.createFromAsset(getAssets(),"fonts/Langdon.otf");
+        title_consejo.setTypeface(face);
+
     }
 }
