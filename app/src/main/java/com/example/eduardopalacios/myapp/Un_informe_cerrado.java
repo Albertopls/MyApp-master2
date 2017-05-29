c4ec14f21636d85510f6d64a36f7ae752715652b
aidag <aida_gil02@hotmail.es>
2017-05-29 03:17:22 -0500

    Redaccion pantallas

---
+++ app/src/main/java/com/example/eduardopalacios/myapp/Un_informe_cerrado.java
@@ -0,0 +1,13 @@
+package com.example.eduardopalacios.myapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Un_informe_cerrado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_un_informe_cerrado);
+    }
+}
